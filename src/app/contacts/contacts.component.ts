6edf0fcf00eadecfa697cd6c11cf4a0ad6c3a32e
parquett <boss.leoneli@mail.ru>
2024-06-27 20:26:20 +0300

    make strictly typed form; use kendo everywhere

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,11 +1,10 @@
-// src/app/contacts/contacts.component.ts
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, Validators, FormControl, NonNullableFormBuilder } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {MatRadioModule} from '@angular/material/radio';
-import { InputsModule } from '@progress/kendo-angular-inputs';
+import { InputsModule, RadioButtonModule } from '@progress/kendo-angular-inputs';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { ReactiveFormsModule } from '@angular/forms';
 
 interface Contact {
   id: number;
@@ -17,39 +16,54 @@ interface Contact {
   status: string;
 }
 
+interface ContactForm {
+  name: FormControl<string>;
+  phone: FormControl<string>;
+  email: FormControl<string>;
+  address: FormControl<string | null>;
+  gender: FormControl<string>;
+  status: FormControl<string>;
+}
+
 @Component({
   selector: 'app-contacts',
   standalone: true,
   imports: [
-    CommonModule, 
+    CommonModule,
     ReactiveFormsModule,
-    MatRadioModule,
     InputsModule,
     ButtonsModule,
-    DropDownsModule
+    DropDownsModule,
+    RadioButtonModule
   ],
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
   contacts: Contact[] = [];
-  contactForm: FormGroup;
+  contactForm!: FormGroup<ContactForm>;
   editMode: boolean = false;
   selectedContact: Contact | null = null;
 
-  constructor(private fb: FormBuilder) {
+  genderOptions = ['Male', 'Female'];
+
+  constructor(private fb: NonNullableFormBuilder) {
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
       phone: ['', Validators.required],
       email: ['', [Validators.email]],
       address: [''],
-      gender: ['', Validators.required],
+      gender: ['Select gender', Validators.required],
       status: ['', Validators.required]
-    });
+    }) as FormGroup<ContactForm>;
   }
 
   ngOnInit() {
     this.loadContacts();
+  }
+
+  get f() {
+    return this.contactForm.controls;
   }
 
   loadContacts() {
@@ -67,10 +81,22 @@ export class ContactsComponent implements OnInit {
     if (this.contactForm.valid) {
       const newContact: Contact = {
         id: this.contacts.length ? Math.max(...this.contacts.map(c => c.id)) + 1 : 1,
-        ...this.contactForm.value
+        name: this.f.name.value,
+        phone: this.f.phone.value,
+        email: this.f.email.value || '',
+        address: this.f.address.value || '',
+        gender: this.f.gender.value,
+        status: this.f.status.value
       };
       this.contacts.push(newContact);
-      this.contactForm.reset();
+      this.contactForm.reset({
+        name: '',
+        phone: '',
+        email: '',
+        address: null,
+        gender: '',
+        status: ''
+      });
       this.saveContacts();
     }
   }
@@ -84,10 +110,26 @@ export class ContactsComponent implements OnInit {
   updateContact() {
     if (this.contactForm.valid && this.selectedContact) {
       const index = this.contacts.findIndex(c => c.id === this.selectedContact!.id);
-      this.contacts[index] = { ...this.selectedContact, ...this.contactForm.value };
+      const updatedContact = {
+        ...this.selectedContact,
+        name: this.f.name.value,
+        phone: this.f.phone.value,
+        email: this.f.email.value || '',
+        address: this.f.address.value || '',
+        gender: this.f.gender.value,
+        status: this.f.status.value
+      };
+      this.contacts[index] = updatedContact;
       this.editMode = false;
       this.selectedContact = null;
-      this.contactForm.reset();
+      this.contactForm.reset({
+        name: '',
+        phone: '',
+        email: '',
+        address: null,
+        gender: '',
+        status: ''
+      });
       this.saveContacts();
     }
   }
@@ -95,6 +137,13 @@ export class ContactsComponent implements OnInit {
   cancelEdit() {
     this.editMode = false;
     this.selectedContact = null;
-    this.contactForm.reset();
+    this.contactForm.reset({
+      name: '',
+      phone: '',
+      email: '',
+      address: null,
+      gender: '',
+      status: ''
+    });
   }
 }
